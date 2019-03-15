b1f443ebdb91630c8ef0f23a1db4b83970b2f4a8
x-zen <jdharris.x@gmail.com>
2019-03-15 18:07:32 -0500

    Vunelrabilty fix at Home component

--- index/src/comp/Home/Home.js
+++ index/src/comp/Home/Home.js
@@ -21,11 +21,11 @@ const Home = ({props}) => (
         <h3>Find me on the web!</h3>
 
         <div className='links'>
-          <a href='https://github.com/x-zen' target='_blank'><i class="fab fa-github-square"></i></a>
-          <a href='https://www.linkedin.com/in/zen-harris-979772180' target='_blank'><i class="fab fa-linkedin"></i></a>
-          <a href='https://twitter.com/zen_stuff' target='_blank'><i class="fab fa-twitter-square"></i></a>
-          <a href='https://www.facebook.com/profile.php?id=100009471423841' target='_blank'><i class="fab fa-facebook-square"></i></a>
-          <a href='mailto:jdharris.x@gmail.com' target='_blank'><i class="fas fa-envelope-square"></i></a>
+          <a rel="noopener noreferrer" href='https://github.com/x-zen' target='_blank'><i class="fab fa-github-square"></i></a>
+          <a rel="noopener noreferrer" href='https://www.linkedin.com/in/zen-harris-979772180' target='_blank'><i class="fab fa-linkedin"></i></a>
+          <a rel="noopener noreferrer" href='https://twitter.com/zen_stuff' target='_blank'><i class="fab fa-twitter-square"></i></a>
+          <a rel="noopener noreferrer" href='https://www.facebook.com/profile.php?id=100009471423841' target='_blank'><i class="fab fa-facebook-square"></i></a>
+          <a rel="noopener noreferrer" href='mailto:jdharris.x@gmail.com' target='_blank'><i class="fas fa-envelope-square"></i></a>
         </div>
       </div>
     </div>
